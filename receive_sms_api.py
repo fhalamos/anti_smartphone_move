72537e86e6d67eb7373e46807c60c412b6d90a21
Felipe Alamos <felipealamos@gmail.com>
2020-03-30 22:05:10 -0500

    API prepared to read request content of sms

--- receive_sms_api.py
+++ receive_sms_api.py
@@ -1,19 +1,24 @@
-# /usr/bin/env python
-# Download the twilio-python library from twilio.com/docs/libraries/python
+#Reference https://www.twilio.com/docs/sms/quickstart/python#receive-and-reply-to-inbound-sms-messages-with-python
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+
+import calendar_requester
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
+def sms_reply():
+    """Respond to incoming messages with a SMS according to the request sent (day for which event are retrieve)"""
+
+    message_body = request.form['Body']
+
     # Start our response
     resp = MessagingResponse()
 
     # Add a message
-    resp.message("Ahoy! Thanks so much for your message!")
-
+    resp.message(calendar_requester.get_events(message_body))
+    
     return str(resp)
 
 if __name__ == "__main__":
